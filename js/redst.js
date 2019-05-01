6bdbe7ebd1c1169f462e6ea23e5835a00d8c606d
zawsx <s8gl2@setz.gq>
2019-05-01 11:10:39 +0100

    redst

---
+++ js/redst.js
@@ -0,0 +1,47 @@
+var sw_targ=true; var ff_targ=''; var qs= querySt('t'); switch(qs) {
+// StART  // bit.ly/stxxx http://codeleftlab.github.io/redir/rdst.htm?t=a22 
+case 'a22'	:ff_targ='https://';				
+ff_targ_text='StART a22'; break; // bit.ly/stm22 https://codeleftlab.github.io/redir/rdst.htm?t=a22					
+case 'a23'	:ff_targ='https://';				
+ff_targ_text='StART a23'; break; // bit.ly/stm23 https://codeleftlab.github.io/redir/rdst.htm?t=a23					
+case 'a24'	:ff_targ='https://';				
+ff_targ_text='StART a24'; break; // bit.ly/stm24 https://codeleftlab.github.io/redir/rdst.htm?t=a24					
+case 'a25'	:ff_targ='https://';				
+ff_targ_text='StART a25'; break; // bit.ly/stm25 https://codeleftlab.github.io/redir/rdst.htm?t=a25					
+case 'a26'	:ff_targ='https://';				
+ff_targ_text='StART a26'; break; // bit.ly/stm26 https://codeleftlab.github.io/redir/rdst.htm?t=a26					
+case 'a27'	:ff_targ='https://';				
+ff_targ_text='StART a27'; break; // bit.ly/stm27 https://codeleftlab.github.io/redir/rdst.htm?t=a27					
+case 'a28'	:ff_targ='https://';				
+ff_targ_text='StART a28'; break; // bit.ly/stm28 https://codeleftlab.github.io/redir/rdst.htm?t=a28					
+case 'a29'	:ff_targ='https://';				
+ff_targ_text='StART a29'; break; // bit.ly/stm29 https://codeleftlab.github.io/redir/rdst.htm?t=a29					
+case 'a30'	:ff_targ='https://';				
+ff_targ_text='StART a30'; break; // bit.ly/stm30 https://codeleftlab.github.io/redir/rdst.htm?t=a30					
+case 'a31'	:ff_targ='https://';				// assigned so far
+ff_targ_text='StART a31'; break; // bit.ly/stm31 https://codeleftlab.github.io/redir/rdst.htm?t=a31					
+case 'a32'	:ff_targ='https://';				
+ff_targ_text='StART a32'; break; // bit.ly/stm32 https://codeleftlab.github.io/redir/rdst.htm?t=a32					
+case 'a33'	:ff_targ='https://';				
+ff_targ_text='StART a33'; break; // bit.ly/stm33 https://codeleftlab.github.io/redir/rdst.htm?t=a33					
+case 'a34'	:ff_targ='https://';				
+ff_targ_text='StART a34'; break; // bit.ly/stm34 https://codeleftlab.github.io/redir/rdst.htm?t=a34					
+case 'a35'	:ff_targ='https://';				
+ff_targ_text='StART a35'; break; // bit.ly/stm35 https://codeleftlab.github.io/redir/rdst.htm?t=a35					
+case 'a36'	:ff_targ='https://';				
+ff_targ_text='StART a36'; break; // bit.ly/stm36 https://codeleftlab.github.io/redir/rdst.htm?t=a36					
+case 'a37'	:ff_targ='https://';				
+ff_targ_text='StART a37'; break; // bit.ly/stm37 https://codeleftlab.github.io/redir/rdst.htm?t=a37					
+case 'a38'	:ff_targ='https://';				
+ff_targ_text='StART a38'; break; // bit.ly/stm38 https://codeleftlab.github.io/redir/rdst.htm?t=a38					
+case 'a39'	:ff_targ='https://';				
+ff_targ_text='StART a39'; break; // bit.ly/stm39 https://codeleftlab.github.io/redir/rdst.htm?t=a39					
+case 'a40'	:ff_targ='https://';				
+ff_targ_text='StART a40'; break; // bit.ly/stm40 https://codeleftlab.github.io/redir/rdst.htm?t=a40									
+
+default:sw_targ=false;break;
+}
+if (sw_targ==true) {window.location.href=ff_targ; document.write('<meta http-equiv="refresh" content="1;url='+ff_targ+'"><title>'+ff_targ_text+'</title>'); }
+else {document.write('There may have been an error - no action possible:'+ff_targ);}
+function querySt(Key) {var url = window.location.href; KeysValues = url.split(/[\?&]+/);
+for (i = 0; i < KeysValues.length; i++) {KeyValue = KeysValues[i].split("="); if (KeyValue[0] == Key) {return KeyValue[1];}}}
