76016dc421a5b627529aa4bd230b6e5d8aef43e5
Chris <zawsx@users.noreply.github.com>
2019-06-07 17:16:18 +0100

    Update redst.js

--- js/redst.js
+++ js/redst.js
@@ -2,7 +2,7 @@ var sw_targ=true; var ff_targ=''; var qs= querySt('t'); switch(qs) {
 // StART  // bit.ly/stxxx http://codeleftlab.github.io/redir/rdst.htm?t=a22 
 case 'a22'	:ff_targ='https://www.dropbox.com/s/q8fgul2voa32jja/190509Healthy%20Streets%20St%20Ann%27s%20-%20Pitch.docx?dl=0';				
 ff_targ_text='Healthy Streets St Anns - pitch'; break; // bit.ly/hesta https://codeleftlab.github.io/redir/rdst.htm?t=a22					
-case 'a23'	:ff_targ='https://www.dropbox.com/s/wg6wv9zh30bup1h/190513DRAFT%20Planning%20Change%20of%20Use%20LBH%20form-Mh28.pdf?dl=0';				
+case 'a23'	:ff_targ='https://www.dropbox.com/s/713ecd99yu4ute6/190610Change%20of%20Use%20to%20B1.pdf?dl=0';				
 ff_targ_text='StART Change of Use form'; break; // http://bit.ly/stmumhcu https://codeleftlab.github.io/redir/rdst.htm?t=a23	
 case 'a24'	:ff_targ='https://www.dropbox.com/s/gc2ahy6hw6fri0f/181101Baseline%20Ecological%20Surveys.docx?dl=0';				
 ff_targ_text='StART a24'; break; // bit.ly/stenvsu https://codeleftlab.github.io/redir/rdst.htm?t=a24					
