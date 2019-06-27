2e69edea49772d31c1340b2224a976872cdf9b3d
Chris <zawsx@users.noreply.github.com>
2019-06-27 15:56:57 +0100

    Update redst1.js

--- js/redst1.js
+++ js/redst1.js
@@ -18,8 +18,8 @@ case 'a29'	:ff_targ='https://docs.google.com/document/d/1z_JOFV5HglpndDTyz3ZumDt
 ff_targ_text='StART Group - consulation response landing page'; break; // bit.ly/stgconsult https://codeleftlab.github.io/redir/rdst.htm?t=a29					
 case 'a30'	:ff_targ='https://www.google.com/maps/d/u/0/viewer?mid=1lSv1CWM3Vp2ACZEHNnHKQFH-sWrrvIA9&ll=51.574036536890226%2C-0.09893717904515142&z=13';				
 ff_targ_text='London Neighbourhood fora'; break; // bit.ly/sznflon https://codeleftlab.github.io/redir/rdst.htm?t=a30					
-case 'a31'	:ff_targ='https://';				
-ff_targ_text='StART a31'; break; // bit.ly/stm31 https://codeleftlab.github.io/redir/rdst.htm?t=a31					
+case 'a31'	:ff_targ='https://docs.google.com/spreadsheets/d/1qNF4TL8WAWXApzpF7QYbvrmwZyscrZiyQVX7VvEtci4/edit?usp=sharing';				
+ff_targ_text='People Not Profit Campaign'; break; // bit.ly/pnpcamp https://codeleftlab.github.io/redir/rdst.htm?t=a31					
 case 'a32'	:ff_targ='https://';				
 ff_targ_text='StART a32'; break; // bit.ly/stm32 https://codeleftlab.github.io/redir/rdst.htm?t=a32					
 case 'a33'	:ff_targ='https://';				
