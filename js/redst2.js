acf1c6781b3fa1f3745e099f5891fdd4dc97496f
Chris <zawsx@users.noreply.github.com>
2020-04-13 12:00:40 +0100

    Update redst2.js

--- js/redst2.js
+++ js/redst2.js
@@ -1,35 +1,35 @@
 var sw_targ=true; var ff_targ=''; var qs= querySt('t'); switch(qs) {
 case 'b22'	:ff_targ='https://www.setz.gq';				
 ff_targ_text='forsalehere Forsalehere ForSaleHere'; break; // bit.ly/forsalehere https://codeleftlab.github.io/redir/rdst2.htm?t=b22					
-case 'b23'	:ff_targ='https://www.dropbox.com/s/713ecd99yu4ute6/190610Change%20of%20Use%20to%20B1.pdf?dl=0';				
+case 'b23'	:ff_targ='2V5wTzd';				
 ff_targ_text='tttttttttttttt'; break; // http://bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b23	
-case 'b24'	:ff_targ='https://www.dropbox.com/s/gc2ahy6hw6fri0f/181101Baseline%20Ecological%20Surveys.docx?dl=0';				
+case 'b24'	:ff_targ='3ekA9Ow';				
 ff_targ_text='tttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b24					
-case 'b25'	:ff_targ='https://www.dropbox.com/s/9s5pwz2o5bbpxlv/190520MHbudget%20and%20actuals.xlsx?dl=0';				
+case 'b25'	:ff_targ='done';				
 ff_targ_text='tttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b25					
-case 'b26'	:ff_targ='https://drive.google.com/file/d/1kyLs3m_kse42HqTArgqfIglE3Bgc9N39/view?usp=sharing';				
+case 'b26'	:ff_targ='done';				
 ff_targ_text='ttttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b26					
-case 'b27'	:ff_targ='https://www.dropbox.com/s/8fyp36z9swxskqx/DRAFT%20StART%20principles%20to%20guide%20meanwhile%20use.docx?dl=0';				
+case 'b27'	:ff_targ='done';				
 ff_targ_text='tttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b27					
 case 'b28'	:ff_targ='bbbbb';				
 ff_targ_text='tttttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b28					
-case 'b29'	:ff_targ='https://docs.google.com/document/d/1z_JOFV5HglpndDTyz3ZumDtIwETarwXpFlI8kbpRbCo/edit?usp=sharing';				
+case 'b29'	:ff_targ='bbbbb';				
 ff_targ_text='ttttttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b29					
-case 'b30'	:ff_targ='https://www.google.com/maps/d/u/0/viewer?mid=1lSv1CWM3Vp2ACZEHNnHKQFH-sWrrvIA9&ll=51.574036536890226%2C-0.09893717904515142&z=13';				
+case 'b30'	:ff_targ='';				
 ff_targ_text='tttttttttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b30					
-case 'b31'	:ff_targ='https://docs.google.com/spreadsheets/d/1qNF4TL8WAWXApzpF7QYbvrmwZyscrZiyQVX7VvEtci4/edit?usp=sharing';				
+case 'b31'	:ff_targ='';				
 ff_targ_text='tttttttttttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b31					
-case 'b32'	:ff_targ='https://docs.google.com/spreadsheets/d/1fnpdu5J7T6JtiOlNLV8i_-bpSVVPAzmUCq_JiBs_FKQ';				
+case 'b32'	:ff_targ='';				
 ff_targ_text='ttttttttttttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b32					
-case 'b33'	:ff_targ='https://www.youtube.com/watch?v=G-MnCu7Jnoo';				
+case 'b33'	:ff_targ='';				
 ff_targ_text='tttttttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b33					
-case 'b34'	:ff_targ='https://';				
+case 'b34'	:ff_targ='';				
 ff_targ_text='text b34'; break; // bit.ly/stm34 https://codeleftlab.github.io/redir/rdst2.htm?t=b34					
-case 'b35'	:ff_targ='https://';				
+case 'b35'	:ff_targ='';				
 ff_targ_text='text b35'; break; // bit.ly/stm35 https://codeleftlab.github.io/redir/rdst2.htm?t=b35					
-case 'b36'	:ff_targ='https://';				
+case 'b36'	:ff_targ='';				
 ff_targ_text='text b36'; break; // bit.ly/stm36 https://codeleftlab.github.io/redir/rdst2.htm?t=b36					
-case 'b37'	:ff_targ='https://';				
+case 'b37'	:ff_targ='  ';				
 ff_targ_text='text b37'; break; // bit.ly/stm37 https://codeleftlab.github.io/redir/rdst2.htm?t=b37					
 case 'b38'	:ff_targ='https://';				
 ff_targ_text='text b38'; break; // bit.ly/stm38 https://codeleftlab.github.io/redir/rdst2.htm?t=b38					
