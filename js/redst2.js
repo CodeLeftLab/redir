619ad9217866d3be52e26a6a662459c9e6dea3e6
Chris <zawsx@users.noreply.github.com>
2020-05-01 09:57:51 +0100

    Update redst2.js

--- js/redst2.js
+++ js/redst2.js
@@ -1,43 +1,40 @@
 var sw_targ=true; var ff_targ=''; var qs= querySt('t'); switch(qs) {
-case 'b22'	:ff_targ='https://www.setz.gq';				
-ff_targ_text='forsalehere Forsalehere ForSaleHere'; break; // bit.ly/forsalehere https://codeleftlab.github.io/redir/rdst2.htm?t=b22					
-case 'b23'	:ff_targ='https://fgmenu.around.gq';				
-ff_targ_text='Finance Group Menu'; break; // http://bit.ly/stfgmenu https://codeleftlab.github.io/redir/rdst2.htm?t=b23	
-    ///////////////////////////
-case 'b24'	:ff_targ='3https://drive.google.com/drive/folders/1rn8I3jvmShCZEPgse2yeLo_sH89dYV5_?usp=sharing';				
-ff_targ_text='StART Finance Group Shared files area'; break; // bit.ly/stfgfiles https://codeleftlab.github.io/redir/rdst2.htm?t=b24					
-case 'b25'	:ff_targ='done';				
-ff_targ_text='tttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b25					
-case 'b26'	:ff_targ='done';				
-ff_targ_text='ttttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b26					
-case 'b27'	:ff_targ='done';				
-ff_targ_text='tttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b27					
-case 'b28'	:ff_targ='bbbbb';				
-ff_targ_text='tttttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b28					
-case 'b29'	:ff_targ='bbbbb';				
-ff_targ_text='ttttttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b29					
-case 'b30'	:ff_targ='';				
-ff_targ_text='tttttttttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b30					
-case 'b31'	:ff_targ='';				
-ff_targ_text='tttttttttttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b31					
-case 'b32'	:ff_targ='';				
-ff_targ_text='ttttttttttttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b32					
-case 'b33'	:ff_targ='';				
-ff_targ_text='tttttttttttttttttttttt'; break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b33					
-case 'b34'	:ff_targ='';				
-ff_targ_text='text b34'; break; // bit.ly/stm34 https://codeleftlab.github.io/redir/rdst2.htm?t=b34					
-case 'b35'	:ff_targ='';				
-ff_targ_text='text b35'; break; // bit.ly/stm35 https://codeleftlab.github.io/redir/rdst2.htm?t=b35					
-case 'b36'	:ff_targ='';				
-ff_targ_text='text b36'; break; // bit.ly/stm36 https://codeleftlab.github.io/redir/rdst2.htm?t=b36					
-case 'b37'	:ff_targ='  ';				
-ff_targ_text='text b37'; break; // bit.ly/stm37 https://codeleftlab.github.io/redir/rdst2.htm?t=b37					
-case 'b38'	:ff_targ='https://';				
-ff_targ_text='text b38'; break; // bit.ly/stm38 https://codeleftlab.github.io/redir/rdst2.htm?t=b38					
-case 'b39'	:ff_targ='https://';				
-ff_targ_text='text b39'; break; // bit.ly/stm39 https://codeleftlab.github.io/redir/rdst2.htm?t=b39					
-case 'b40'	:ff_targ='https://';				
-ff_targ_text='text b40'; break; // bit.ly/stm40 https://codeleftlab.github.io/redir/rdst2.htm?t=b40									
+case 'b22'	:ff_targ_text='forsalehere Forsalehere ForSaleHere';  ff_targ='https://www.setz.gq'; break; // bit.ly/forsalehere https://codeleftlab.github.io/redir/rdst2.htm?t=b22					
+case 'b23'	: ff_targ_text='Finance Group Menu'; ff_targ='https://fgmenu.around.gq';break;  // http://bit.ly/stfgmenu https://codeleftlab.github.io/redir/rdst2.htm?t=b23	
+case 'b24'	:ff_targ_text='StART Finance Group Shared files area';  ff_targ='3https://drive.google.com/drive/folders/1rn8I3jvmShCZEPgse2yeLo_sH89dYV5_?usp=sharing';break; 
+        // bit.ly/stfgfiles https://codeleftlab.github.io/redir/rdst2.htm?t=b24					
+case 'b25'	:ff_targ_text='tttttttttttttt'; ff_targ='done';break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b25					
+case 'b26'	:ff_targ='done'; ff_targ_text='tttttttttttttt'; break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b26					
+case 'b27'	:ff_targ_text='tttttttttttttttttt'; ff_targ='done'; break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b27					
+case 'b28'	:ff_targ_text='tttttttttttttttttt'; ff_targ='bbbbb'; break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b28					
+case 'b29'	:ff_targ_text='tttttttttttttttttt'; ff_targ='bbbbb';break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b29					
+case 'b30'	:ff_targ_text='tttttttttttttttttt'; ff_targ=''; break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b30					
+case 'b31'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b31					
+case 'b32'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	
+        break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b32					
+case 'b33'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	
+        break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdst2.htm?t=b33					
+case 'b34'	:ff_targ_text='tttttttttttttttttt'; ff_targ=''; break; 
+        // bit.ly/stm34 https://codeleftlab.github.io/redir/rdst2.htm?t=b34					
+case 'b35'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	break; 
+        // bit.ly/stm35 https://codeleftlab.github.io/redir/rdst2.htm?t=b35					
+case 'b36'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	break; 
+        // bit.ly/stm36 https://codeleftlab.github.io/redir/rdst2.htm?t=b36					
+case 'b37'	:ff_targ_text='tttttttttttttttttt'; ff_targ='  '; break; 
+        // bit.ly/stm37 https://codeleftlab.github.io/redir/rdst2.htm?t=b37					
+case 'b38'	:ff_targ_text='tttttttttttttttttt'; ff_targ='https://';	break; 
+        // bit.ly/stm38 https://codeleftlab.github.io/redir/rdst2.htm?t=b38					
+case 'b39'	:ff_targ_text='tttttttttttttttttt'; ff_targ='https://';	break; 
+        // bit.ly/stm39 https://codeleftlab.github.io/redir/rdst2.htm?t=b39					
+case 'b40'	:ff_targ_text='tttttttttttttttttt'; ff_targ='https://';	break; 
+        // bit.ly/stm40 https://codeleftlab.github.io/redir/rdst2.htm?t=b40									
 
 default:sw_targ=false;break;
 }
