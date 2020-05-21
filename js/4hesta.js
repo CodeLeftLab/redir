beab93944f341b79790c060f5d0637bbfb230d1e
Chris <zawsx@users.noreply.github.com>
2020-05-21 10:33:11 +0100

    Update 4hesta.js

--- js/4hesta.js
+++ js/4hesta.js
@@ -1,40 +1,40 @@
 var sw_targ=true; var ff_targ=''; var qs= querySt('t'); switch(qs) {
-case 'he22'	:ff_targ_text='xxxxxxxxxxxxxx';  ff_targ='https://www.setz.gq'; break; // bit.ly/xxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=he22					
-case 'he23'	: ff_targ_text='xxxxxxxxx'; ff_targ='https://fgmenu.around.gq';break;  // http://bit.ly/xxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=he23	
-case 'he24'	:ff_targ_text='xxxxxxxxxxxxxxx';  ff_targ='xxxxxxxxxxxxxxxxxxxx';break; 
-        // bit.ly/stfgfiles https://codeleftlab.github.io/redir/red4hesta.htm?h=he24					
-case 'he25'	:ff_targ_text='tttttttttttttt'; ff_targ='done';break; 
-        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=he25					
-case 'he26'	:ff_targ='done'; ff_targ_text='tttttttttttttt'; break; 
-        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=he26					
-case 'he27'	:ff_targ_text='tttttttttttttttttt'; ff_targ='done'; break; 
-        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=he27					
-case 'he28'	:ff_targ_text='tttttttttttttttttt'; ff_targ='bbbbb'; break; 
-        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=he28					
-case 'he29'	:ff_targ_text='tttttttttttttttttt'; ff_targ='bbbbb';break; 
-        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=he29					
-case 'he30'	:ff_targ_text='tttttttttttttttttt'; ff_targ=''; break; 
-        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=he30					
-case 'he31'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';break; 
-        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=he31					
-case 'he32'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	
+case 'h22'	:ff_targ_text='xxxxxxxxxxxxxx';  ff_targ='https://www.setz.gq'; break; // bit.ly/xxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h22					
+case 'h23'	: ff_targ_text='xxxxxxxxx'; ff_targ='https://fgmenu.around.gq';break;  // http://bit.ly/xxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h23	
+case 'h24'	:ff_targ_text='xxxxxxxxxxxxxxx';  ff_targ='xxxxxxxxxxxxxxxxxxxx';break; 
+        // bit.ly/stfgfiles https://codeleftlab.github.io/redir/red4hesta.htm?h=h24					
+case 'h25'	:ff_targ_text='tttttttttttttt'; ff_targ='done';break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h25					
+case 'h26'	:ff_targ='done'; ff_targ_text='tttttttttttttt'; break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h26					
+case 'h27'	:ff_targ_text='tttttttttttttttttt'; ff_targ='done'; break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h27					
+case 'h28'	:ff_targ_text='tttttttttttttttttt'; ff_targ='bbbbb'; break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h28					
+case 'h29'	:ff_targ_text='tttttttttttttttttt'; ff_targ='bbbbb';break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h29					
+case 'h30'	:ff_targ_text='tttttttttttttttttt'; ff_targ=''; break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h30					
+case 'h31'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h31					
+case 'h32'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	
         break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?t=he32					
-case 'he33'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	
-        break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=he33					
-case 'he34'	:ff_targ_text='tttttttttttttttttt'; ff_targ=''; break; 
-        // bit.ly/stm34 https://codeleftlab.github.io/redir/red4hesta.htm?h=he34					
-case 'he35'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	break; 
-        // bit.ly/stm35 https://codeleftlab.github.io/redir/red4hesta.htm?h=he35					
-case 'he36'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	break; 
-        // bit.ly/stm36 https://codeleftlab.github.io/redir/red4hesta.htm?h=he36					
-case 'he37'	:ff_targ_text='tttttttttttttttttt'; ff_targ='  '; break; 
-        // bit.ly/stm37 https://codeleftlab.github.io/redir/red4hesta.htm?h=he37					
-case 'he38'	:ff_targ_text='tttttttttttttttttt'; ff_targ='https://';	break; 
-        // bit.ly/stm38 https://codeleftlab.github.io/redir/red4hesta.htm?h=he38					
-case 'he39'	:ff_targ_text='tttttttttttttttttt'; ff_targ='https://';	break; 
-        // bit.ly/stm39 https://codeleftlab.github.io/redir/red4hesta.htm?h=he39					
-case 'he40'	:ff_targ_text='tttttttttttttttttt'; ff_targ='https://';	break; 
-        // bit.ly/stm40 https://codeleftlab.github.io/redir/red4hesta.htm?h=he40									
+case 'h33'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	
+        break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h33					
+case 'h34'	:ff_targ_text='tttttttttttttttttt'; ff_targ=''; break; 
+        // bit.ly/stm34 https://codeleftlab.github.io/redir/red4hesta.htm?h=h34					
+case 'h35'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	break; 
+        // bit.ly/stm35 https://codeleftlab.github.io/redir/red4hesta.htm?h=h35					
+case 'h36'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	break; 
+        // bit.ly/stm36 https://codeleftlab.github.io/redir/red4hesta.htm?h=h36					
+case 'h37'	:ff_targ_text='tttttttttttttttttt'; ff_targ='  '; break; 
+        // bit.ly/stm37 https://codeleftlab.github.io/redir/red4hesta.htm?h=h37					
+case 'h38'	:ff_targ_text='tttttttttttttttttt'; ff_targ='https://';	break; 
+        // bit.ly/stm38 https://codeleftlab.github.io/redir/red4hesta.htm?h=h38					
+case 'h39'	:ff_targ_text='tttttttttttttttttt'; ff_targ='https://';	break; 
+        // bit.ly/stm39 https://codeleftlab.github.io/redir/red4hesta.htm?h=h39					
+case 'h40'	:ff_targ_text='tttttttttttttttttt'; ff_targ='https://';	break; 
+        // bit.ly/stm40 https://codeleftlab.github.io/redir/red4hesta.htm?h=h40									
 
 default:sw_targ=false;break;
 }
