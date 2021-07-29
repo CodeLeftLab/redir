0236703df235f1642b30f7193bd8e5f47b6b9a80
zawsx <certain456+zxghgit@gmail.com>
2021-07-29 19:50:45 +0100

    Update rdb2.js

--- js/rdb2.js
+++ js/rdb2.js
@@ -1,40 +1,37 @@
-var sw_targ=true; var ff_targ=''; var qs= querySt('h'); switch(qs) {
-case 'h22'	:ff_targ_text='xxxxx';  ff_targ='https://docs.google.com/document/d/1khySk_cvWLl0tNugqsdiK5e0za9V9TGwh7HIk8EVt7w'; break; // bit.ly/hestacg https://codeleftlab.github.io/redir/red4hesta.htm?h=h22					
-case 'h23'	:ff_targ_text='xxxxx'; ff_targ='https://fgmenu.around.gq';break;  // http://bit.ly/xxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h23	
-case 'h24'	:ff_targ_text='xxxxx';  ff_targ='xxxxxxxxxxxxxx';break; 
-        // bit.ly/stfgfiles https://codeleftlab.github.io/redir/red4hesta.htm?h=h24					
-case 'h25'	:ff_targ_text='tttttttttttttt'; ff_targ='done';break; 
-        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h25					
-case 'h26'	:ff_targ='done'; ff_targ_text='tttttttttttttt'; break; 
-        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h26					
-case 'h27'	:ff_targ_text='tttttttttttttttttt'; ff_targ='done'; break; 
-        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h27					
-case 'h28'	:ff_targ_text='tttttttttttttttttt'; ff_targ='bbbbb'; break; 
-        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h28					
-case 'h29'	:ff_targ_text='tttttttttttttttttt'; ff_targ='bbbbb';break; 
-        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h29					
-case 'h30'	:ff_targ_text='tttttttttttttttttt'; ff_targ=''; break; 
-        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h30					
-case 'h31'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';break; 
-        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h31					
-case 'h32'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	
-        break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?t=he32					
-case 'h33'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	
-        break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/red4hesta.htm?h=h33					
-case 'h34'	:ff_targ_text='tttttttttttttttttt'; ff_targ=''; break; 
-        // bit.ly/stm34 https://codeleftlab.github.io/redir/red4hesta.htm?h=h34					
-case 'h35'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	break; 
-        // bit.ly/stm35 https://codeleftlab.github.io/redir/red4hesta.htm?h=h35					
-case 'h36'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	break; 
-        // bit.ly/stm36 https://codeleftlab.github.io/redir/red4hesta.htm?h=h36					
-case 'h37'	:ff_targ_text='tttttttttttttttttt'; ff_targ='  '; break; 
-        // bit.ly/stm37 https://codeleftlab.github.io/redir/red4hesta.htm?h=h37					
-case 'h38'	:ff_targ_text='tttttttttttttttttt'; ff_targ='https://';	break; 
-        // bit.ly/stm38 https://codeleftlab.github.io/redir/red4hesta.htm?h=h38					
-case 'h39'	:ff_targ_text='tttttttttttttttttt'; ff_targ='https://';	break; 
-        // bit.ly/stm39 https://codeleftlab.github.io/redir/red4hesta.htm?h=h39					
-case 'h40'	:ff_targ_text='tttttttttttttttttt'; ff_targ='https://';	break; 
-        // bit.ly/stm40 https://codeleftlab.github.io/redir/red4hesta.htm?h=h40									
+var sw_targ=true; var ff_targ=''; var qs= querySt('b'); switch(qs) {
+case '1'	:ff_targ_text='Broadwater Farm Facebook Group';  ff_targ='bttps://docs.google.com/document/d/1khySk_cvWLl0tNugqsdiK5e0za9V9TGwh7HIk8EVt7w'; break; // bit.ly/bwfarm2 https://codeleftlab.github.io/redir/rdb2.htm?b=1		
+case 'b22'	:ff_targ_text='xxxxx';  ff_targ='xxxxxxxxxxxxxx';break; 
+        // bit.ly/stfgfiles https://codeleftlab.github.io/redir/rdb2.htm?b=b22				
+case 'b23'	:ff_targ_text='xxxxx'; ff_targ='bttps://fgmenu.around.gq';break;  // http://bit.ly/xxxx https://codeleftlab.github.io/redir/rdb2.htm?b=b23	
+case 'b24'	:ff_targ_text='xxxxx';  ff_targ='xxxxxxxxxxxxxx';break; 
+        // bit.ly/stfgfiles https://codeleftlab.github.io/redir/rdb2.htm?b=b24					
+case 'b25'	:ff_targ_text='tttttttttttttt'; ff_targ='done';break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdb2.htm?b=b25					
+case 'b26'	:ff_targ='done'; ff_targ_text='tttttttttttttt'; break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdb2.htm?b=b26					
+case 'b27'	:ff_targ_text='tttttttttttttttttt'; ff_targ='done'; break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdb2.htm?b=b27					
+case 'b28'	:ff_targ_text='tttttttttttttttttt'; ff_targ='bbbbb'; break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdb2.htm?b=b28					
+case 'b29'	:ff_targ_text='tttttttttttttttttt'; ff_targ='bbbbb';break; 
+        // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdb2.htm?b=b29					
+case 'b32'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	
+        break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdb2.htm?t=be32					
+case 'b33'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	
+        break; // bit.ly/xxxxxx https://codeleftlab.github.io/redir/rdb2.htm?b=b33					
+case 'b34'	:ff_targ_text='tttttttttttttttttt'; ff_targ=''; break; 
+        // bit.ly/stm34 https://codeleftlab.github.io/redir/rdb2.htm?b=b34					
+case 'b35'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	break; 
+        // bit.ly/stm35 https://codeleftlab.github.io/redir/rdb2.htm?b=b35					
+case 'b36'	:ff_targ_text='tttttttttttttttttt'; ff_targ='';	break; 
+        // bit.ly/stm36 https://codeleftlab.github.io/redir/rdb2.htm?b=b36					
+case 'b37'	:ff_targ_text='tttttttttttttttttt'; ff_targ='  '; break; 
+        // bit.ly/stm37 https://codeleftlab.github.io/redir/rdb2.htm?b=b37					
+case 'b38'	:ff_targ_text='tttttttttttttttttt'; ff_targ='bttps://';	break; 
+        // bit.ly/stm38 https://codeleftlab.github.io/redir/rdb2.htm?b=b38					
+case 'b39'	:ff_targ_text='tttttttttttttttttt'; ff_targ='bttps://';	break; 
+        // bit.ly/stm39 https://codeleftlab.github.io/redir/rdb2.htm?b=b39					
+					
 
 default:sw_targ=false;break;
 }
