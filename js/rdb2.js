5752c674084dc62d3d92770fbe0d3d3ff1dac858
zawsx <certain456+zxghgit@gmail.com>
2021-07-29 20:02:05 +0100

    Update rdb2.js

--- js/rdb2.js
+++ js/rdb2.js
@@ -1,7 +1,8 @@
 var sw_targ=true; var ff_targ=''; var qs= querySt('b'); switch(qs) {
 case 1	:ff_targ_text='Broadwater Farm Facebook Group';  ff_targ='https://docs.google.com/document/d/1khySk_cvWLl0tNugqsdiK5e0za9V9TGwh7HIk8EVt7w'; break;
- // bit.ly/bwfarm2 https://codeleftlab.github.io/redir/rdb2.htm?b=1		
-case 'b22'	:ff_targ_text='xxxxx';  ff_targ='xxxxxxxxxxxxxx';break; 
+ // bit.ly/bwfarm2
+// https://codeleftlab.github.io/redir/rdb2.htm?b=1	
+case 'b22'	:ff_targ_text='xxxxx';  ff_targ='www.ibm.com';break; 
         // bit.ly/stfgfiles https://codeleftlab.github.io/redir/rdb2.htm?b=b22				
 case 'b23'	:ff_targ_text='xxxxx'; ff_targ='https://fgmenu.around.gq';break;  // http://bit.ly/xxxx https://codeleftlab.github.io/redir/rdb2.htm?b=b23	
 case 'b24'	:ff_targ_text='xxxxx';  ff_targ='xxxxxxxxxxxxxx';break; 
@@ -38,6 +39,6 @@ default:sw_targ=false;break;
 }
 if (sw_targ==true) {document.write('<meta http-equiv="refresh" content="1;url='+ff_targ+'"><title>'+ff_targ_text+'</title>');
                    window.location.href=ff_targ;}
-else {document.write('There may have been an error - no action possible. qs='+qs+'<-targ=:'+ff_targ+'<');}
+else {document.write('There may have been an error - no action possible. qs=='+qs+'== targ=='+ff_targ+'==');}
 function querySt(Key) {var url = window.location.href; KeysValues = url.split(/[\?&]+/);
 for (i = 0; i < KeysValues.length; i++) {KeyValue = KeysValues[i].split("="); if (KeyValue[0] == Key) {return KeyValue[1];}}}
