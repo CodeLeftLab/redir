2361fe2dc8749d623fad4541682c62af83c07eaa
Chris <zawsx@users.noreply.github.com>
2022-03-26 09:52:04 +0000

    Update rdb2.js

--- js/rdb2.js
+++ js/rdb2.js
@@ -1,10 +1,10 @@
 var sw_targ=true; var ff_targ=''; var qs= querySt('b'); switch(qs) {
 case '1'	:ff_targ_text='Broadwater Farm Facebook Group';  ff_targ='https://broadwaterfarm.mailchimpsites.com/welcome'; break;
- // https://bit.ly/bwfarm2 // https://codeleftlab.github.io/redir/rdb2.htm?b=1	
+    // https://bit.ly/bwfarm2 // https://codeleftlab.github.io/redir/rdb2.htm?b=1	
 case '2'	:ff_targ_text='BWF Gmap';  ff_targ='https://www.google.co.uk/maps/@51.5911706,-0.0814654,467a,35y,39.34t/data=!3m1!1e3';break;
-        // bit.ly/bwfgmap https://codeleftlab.github.io/redir/rdb2.htm?b=2			
+    // bit.ly/bwfgmap https://codeleftlab.github.io/redir/rdb2.htm?b=2			
 case 'b23'	:ff_targ_text='Social Rent Calculator'; ff_targ='https://around-rcalc.netlify.app/';break;  
-// http://bit.ly/calcrent  CALCRENT https://codeleftlab.github.io/redir/rdb2.htm?b=b23	
+    // http://bit.ly/calcrent  CALCRENT https://codeleftlab.github.io/redir/rdb2.htm?b=b23	
 case 'b24'	:ff_targ_text='xxxxx';  ff_targ='xxxxxxxxxxxxxx';break; 
         // bit.ly/stfgfiles https://codeleftlab.github.io/redir/rdb2.htm?b=b24					
 case 'b25'	:ff_targ_text='tttttttttttttt'; ff_targ='done';break; 
